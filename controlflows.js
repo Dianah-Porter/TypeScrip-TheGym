b0767589e82743651c68aa45cee142ca616aba11
Diane <dianahporter@gmail.com>
2026-01-23 16:45:56 +0200

    feat: Control flows

--- controlflows.js
+++ controlflows.js
@@ -1,32 +1,26 @@
-let quantity: number = 35;
-
+var quantity = 35;
 if (quantity > 0) {
     console.log("Positive number");
 }
-
 else if (quantity < 0) {
     console.log("Negative number");
 }
 else {
     console.log("Zero");
 }
-
 // Output: Positive number
-
-
-let score: number = 96;
-
+var score = 96;
 if (score >= 40) {
     console.log("You passed the examination.");
 }
-
-let fruits: string[] = ["banana", "pineapple", "Orange"];
-for(let fruit of fruits) console.log(fruit);
-
-
+var fruits = ["banana", "pineapple", "Orange"];
+for (var _i = 0, fruits_1 = fruits; _i < fruits_1.length; _i++) {
+    var fruit = fruits_1[_i];
+    console.log(fruit);
+}
 //while loop 
-let num: number = 1;
-while(num  < 7){
+var num = 1;
+while (num < 7) {
     console.log(num);
     num++;
 }
